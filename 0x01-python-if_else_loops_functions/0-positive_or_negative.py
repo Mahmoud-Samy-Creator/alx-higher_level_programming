e7d0241e521e2289bc41bb2cb8946b498d91d804
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-10 01:54:25 +0300

    A python script to tell the var is -ve or +ve or zero

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,5 +5,7 @@ if number > 0:
     print("{} is positive".format(number))
 elif number < 0:
     print("{} is negative".format(number))
-else:
+elif:
     print("{} is zero")
+else:
+    print("wrong type")
