5863b8d0a67a1a96958cae8b91b99f780ec3009c
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-11 05:08:53 +0300

    Write a program that acts like a simple calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
     if argv[2] == '+':
         print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
     elif argv[2] == '-':
