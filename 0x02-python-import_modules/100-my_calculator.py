18dfa8eb0295b3e2c59b3be5822ca97e2fdaa947
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-11 05:09:33 +0300

    Write a program that acts like a simple calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,13 +8,13 @@ if __name__ == "__main__":
     a = int(argv[1])
     b = int(argv[3])
     if argv[2] == '+':
-        print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+        print("{} + {} = {}".format(argv[1], argv[3], add(a, b)))
     elif argv[2] == '-':
-        print("{} - {} = {}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+        print("{} - {} = {}".format(argv[1], argv[3], sub(a, b)))
     elif argv[2] == '*':
-        print("{} * {} = {}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+        print("{} * {} = {}".format(argv[1], argv[3], mul(a, b)))
     elif argv[2] == '/':
-        print("{} / {} = {}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+        print("{} / {} = {}".format(argv[1], argv[3], div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
