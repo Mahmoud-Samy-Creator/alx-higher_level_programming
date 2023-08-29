a071f634293d3e9fbd24b48ddbec35addc34b6cf
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-29 03:25:46 +0300

    A function to safe print integer list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    len = 0
+    for item in my_list:
+        len += 1
+
+    while i < x and i < len:
+        try:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                i += 1
+        except IndexError:
+            print("IndexError: list index out of range")
+            break
+    print("")
+    return i
