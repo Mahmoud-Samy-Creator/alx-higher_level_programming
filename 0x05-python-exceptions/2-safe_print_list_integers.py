53cca0fd709202387c9144ff4d4d5aad65030e51
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-29 03:36:38 +0300

    A function to safe print integer list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,17 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
     len = 0
-    for item in my_list:
-        len += 1
-
-    while i < x and i < len:
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            i += 1
+            len += 1
         except (TypeError, ValueError):
             continue
-        except IndexError:
-            print("IndexError: list index out of range")
-            break
     print("")
-    return (i)
+    return (len)
