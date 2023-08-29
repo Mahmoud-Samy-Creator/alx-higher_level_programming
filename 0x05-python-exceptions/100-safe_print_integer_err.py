86fff01dae64a9d66c4703e5639a7228de3dfb32
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-29 05:35:26 +0300

    A function to saftly print an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from sys import stderr
+import sys
 def safe_print_integer_err(value):
     init = True
     try:
         print("{:d}".format(value))
     except Exception as e:
-        print("Exception: ", e, file=stderr)
+        print("Exception: ", e, file=sys.stderr)
         init = False
     return init
