0b5787652df2d2befa56925e83019687146ba36d
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-31 20:43:27 +0300

    Task(4)

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Square class to define a square"""
+
+
+class Square:
+    """Initializing values"""
+    def __init__(self, size=0):
+        '''Creatign a private attribute'''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''A function return area of square'''
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
