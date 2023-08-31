9da5855e8fd1ab75d96c8cda0d67b0e51436f6dc
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-31 09:43:14 +0300

    Task(1)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class to define a square"""
+
+
+class Square:
+    """Initializing values"""
+    def __init__(self, size):
+        '''Creatign a private attribute'''
+        self.__size = size
