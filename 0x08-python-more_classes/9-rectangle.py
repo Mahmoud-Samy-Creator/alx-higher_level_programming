1094464b9ad99a795246c094e4646096c78c1660
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-06 16:08:28 +0300

    Task[9]

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -132,6 +132,7 @@ class Rectangle:
         type(self).number_of_instances -= 1
         print("{:s}".format("Bye rectangle..."))
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Computes the area of two rectangles and compares them.
 
