fe854a1322ed61895b0c01b32764ea48f1af1000
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-11 19:26:31 +0300

    Task[1]

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,18 +7,11 @@ Module of class inheritance
 """
 
 
-class list:
-    def __init__(self):
-        self.new_list = []
-
-
 class MyList(list):
-    def __str__(self):
-        return f"{self.new_list}"
-
-    def append(self, obj):
-        self.new_list.append(obj)
+    """Class with method print_sorted"""
+    pass
 
     def print_sorted(self):
-        sorted_list = sorted(self.new_list)
-        print(sorted_list)
+        """Methot that sorted a list"""
+
+        print(sorted(list(self)))
