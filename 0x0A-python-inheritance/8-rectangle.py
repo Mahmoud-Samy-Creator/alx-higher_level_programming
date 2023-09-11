5b2d5c292f7f8a6b5e9c04ce1ee72c66c43acdd1
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-11 21:20:42 +0300

    Task[8]

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+class Rectangle(BaseGeometry):
+    '''
+        Rectangle a rectangle
+    '''
+    def __init__(self, width, heihgt):
+        self.integer_validator("width", width)
+        self.integer_validator("height", heihgt)
+
+        self.__width = width
+        self.__heihgt = heihgt
