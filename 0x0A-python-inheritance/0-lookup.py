29f2d7db30308802af90c9048bb2101fb3338deb
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-11 18:52:56 +0300

    Task[0]

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 """
-========================
-A file for lookip module
-========================
+=========================
+Module with lookup method
+=========================
 """
+
+
 def lookup(obj):
     """Returns a list of available attributes and methods"""
-    return [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
+    return dir(obj)
