ce5ad8c4b7ce47fbad1ba182b84d10d913ce8bba
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-12 22:21:45 +0300

    Task[3]

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 
 
-import json
-
 """ A module contains to_json_string method
 """
+
+
+import json
 
 
 def to_json_string(my_obj):
     """A function returns json representation of an object
     Args:
         object: The object to be serialized
+    Return:
+        JSON representation
     """
-    return json.dump(my_obj)
+    return json.dumps(my_obj)
