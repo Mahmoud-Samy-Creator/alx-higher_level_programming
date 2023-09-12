73872475a092d63ef0ff6515a9fb618accf45f5d
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-12 21:37:39 +0300

    Task[0]

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,6 +9,6 @@ def read_file(filename=""):
     Args:
         filename (str, optional): The name of the file to read
     """
-    with open(filename, "r", encoding="utf-8") as file:
+    with open(filename, "r", encoding="UTF-8") as file:
         read_data = file.read()
-        print(read_data)
+        print(read_data, end="")
