c061e3b41f4cff4a85b900e2d226eeb9924b81e2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-14 17:27:40 +0300

    Task[7]

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-
-""" A python script to add arguments to a list
 """
-
-import json
+7-add_item module
+"""
 import sys
+import json
+import os.path
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
-json_list = []
 file = "add_item.json"
+json_list = []
 
-json_list = load(file)
+if os.path.exists(file):
+    json_list = load(file)
 
 for i in range(1, len(sys.argv)):
-        json_list.append(sys.argv[i])
+    json_list.append(sys.argv[i])
 
 save(json_list, file)
