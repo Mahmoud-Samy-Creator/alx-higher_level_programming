e5ec9439ad3822f3be1952c4949357e12f2294d0
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-17 21:05:51 +0300

    Task[9]

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,7 +77,7 @@ class Rectangle(Base):
     def __str__(self):
         return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         if args and len(args) != 0:
             order = 0
             for arg in args:
@@ -95,3 +95,19 @@ class Rectangle(Base):
                 if order == 4:
                     self.y = arg
                 order += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                if k == "width":
+                    self.width = v
+                if k == "height":
+                    self.height = v
+                if k == "x":
+                    self.x = v
+                if k == "y":
+                    self.y = v
