4055f9aec9669e7c39ea23ce4356a2d03a3627b1
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-17 20:26:28 +0300

    Task[1]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,15 +2,14 @@
 
 class Base:
     """
-    The Base class provides a mechanism for generating unique IDs for objects.
+    Provides unique ID generation for objects.
 
     Attributes:
-        __nb_objects (int): A private class variable to keep track of the number of instances created.
-        id (int): An instance variable representing the unique ID of an object.
+        __nb_objects (int): Tracks the number of instances created.
+        id (int): Represents the object's unique ID.
 
     Methods:
-        __init__(self, id=None): Initializes a Base object. If an ID is provided, it uses that ID; 
-        otherwise, it auto-generates a unique ID.
+        __init__(self, id=None): Initializes a Base object.
     """
 
     __nb_objects = 0
@@ -20,11 +19,7 @@ class Base:
         Initializes a Base object.
 
         Args:
-            id (int, optional): An optional ID to assign to the object. If not provided, a unique ID will be generated.
-
-        Note:
-            If an ID is provided, it will be used. If no ID is provided, a unique ID will be assigned, 
-            starting from 1 and incrementing for each new object created.
+            id (int, optional): ID to assign.
         """
         if id:
             self.id = id
