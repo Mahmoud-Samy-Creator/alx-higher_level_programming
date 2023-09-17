37684f6ffb45dc70c09f4ee70441369f3eb7b464
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-17 21:47:21 +0300

    Task[12]

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,3 +18,33 @@ class Square(Rectangle):
     def __str__(self):
         return(f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
 
+    def update(self, *args, **kwargs):
+        if args and len(args) != 0:
+            order = 0
+            for arg in args:
+                if order == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                if order == 1:
+                    self.size = arg
+                if order == 2:
+                    self.x = arg
+                if order == 3:
+                    self.y == arg
+            order += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                if k == "size":
+                    self.size = v
+                if k == "x":
+                    self.x = v
+                if k == "y":
+                    self.y = v
