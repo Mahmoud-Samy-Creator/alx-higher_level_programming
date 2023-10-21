554e5951e32ce2e45ce7f46c98774b5a01f5ab8c
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-10-21 16:04:26 +0000

    7. Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
--- creates at database cities and table inside database
-CREATE database IF NOT EXISTS hbtn_0d_usa;
+-- Creates at database cities and table inside database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE table IF NOT EXISTS cities
-       (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-       state_id INT NOT NULL,
-       name VARCHAR(256) NOT NULL,
-       FOREIGN KEY (state_id) REFERENCES states(id)
+CREATE TABLE IF NOT EXISTS cities
+(
+       id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+       state_id INT NOT NULL FORIGN KEY(id) REFERENCES states(id),
+       name VARCHAR(256) NOT NULL
 );
