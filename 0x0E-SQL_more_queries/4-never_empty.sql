b13cc1c19da8c384217a26f6c2fdca9b4a2b19a0
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-10-21 15:03:14 +0000

    4. ID can't be null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,6 @@
--- creates a table force_name
-CREATE table IF NOT EXISTS id_not_null(id INT NOT NULL DEFAULT 1, name VARCHAR(256));
+-- Table creation with default columns value 1
+CREATE TABLE IF NOT EXISTS id_not_null
+(
+    id INT NOT NULL DEFAULT 1,
+    name VARCHAR(256)
+);
