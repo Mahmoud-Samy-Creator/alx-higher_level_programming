8e85e005924ccb4fd6285fa3c2393e566881a0fb
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-10-21 15:31:21 +0000

    5. Unique ID

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,6 @@
--- creates a table force_name
-CREATE table IF NOT EXISTS unique_id(id INT NOT NULL DEFAULT 1 UNIQUE, name VARCHAR(256));
+-- Create unique_id table with unique attribute
+CREATE TABLE IF NOT EXISTS unique_id
+(
+    id INT NOT NULL UNIQUE DEFAULT 1,
+    name VARCHAR(256)
+);
