3b30a7ef48bc10213552c7404a2f89196615668f
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-14 20:16:45 +0000

    6. Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,9 +2,10 @@
 
 // script to print strings using a loop
 
-let i;
+let i = 0;
 const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (i = 0; i < 3; i++) {
+while (myString[i]) {
   console.log(myString[i]);
+  i++;
 }
