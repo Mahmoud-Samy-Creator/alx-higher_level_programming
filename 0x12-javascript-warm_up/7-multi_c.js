e8258a171d132528edf1b793bf8702babd85aab5
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-14 20:25:11 +0000

    7. I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,17 @@
 #!/usr/bin/node
 
-// Script to print ints passed as arguments to the script.
+// Script to prints x times “C is fun”
 
-const myArg = process.argv[2];
+const num = parseInt(process.argv[2]);
 
-if (isNaN(Number(myArg))) {
-  console.log('Missing number of occurences');
-} else {
-  for (let i = 0; i < myArg; i++) {
-    console.log('C is fun');
+function printing (num) {
+  if (num > 0) {
+    let i;
+
+    for (i = 0; i < num; i++) {
+      console.log('C is fun');
+    }
   }
 }
+
+printing(num);
