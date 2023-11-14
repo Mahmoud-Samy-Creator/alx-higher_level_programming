15fb86add67d067ad4012b27618167d4287ce5ad
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-14 20:47:22 +0000

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,16 +2,24 @@
 
 // Script to print square of size argv[2].
 
-const myArg = process.argv[2];
-let rows = 'X';
+const arg = parseInt(process.argv[2]);
 
-if (isNaN(Number(myArg))) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < myArg - 1; i++) {
-    rows += 'X';
-  }
-  for (let j = 0; j < myArg; j++) {
-    console.log(rows);
+function squarePrint (arg) {
+  if (!arg) {
+    console.log('Missing size');
+  } else {
+    let row = 'X';
+    let i;
+    let j;
+
+    for (i = 0; i < arg; i++) {
+      for (j = 0; j < arg - 1; j++) {
+        row = row + 'X';
+      }
+      console.log(row);
+      row = 'X';
+    }
   }
 }
+
+squarePrint(arg);
