a69bb84b892c25eed45d0fb98ffa64f3c9c7055e
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-14 20:00:48 +0000

    4. Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
-// Script that prints "JavaScript is amazing".
+// Script that prints two arguments passed to it, in the following format: “ is ”
 
-console.log(process.argv[2] + ' is ' + process.argv[3]);
+const args = `${process.argv[2]} is ${process.argv[3]}`;
+console.log(args);
