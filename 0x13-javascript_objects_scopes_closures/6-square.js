5ddc4a1002785699266f0476b136dfe455ea7d31
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-15 21:45:52 +0000

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-// script that implements the class square from 5-square
+/* class Square that defines a square and inherits from Square of 5-square.js */
 
-const Sq1 = require('./5-square');
+const mainSquare = require('./5-square');
 
-module.exports = class Square extends Sq1 {
+module.exports = class Square extends mainSquare {
   charPrint (c) {
     let rows;
     if (c === undefined) {
@@ -22,4 +22,4 @@ module.exports = class Square extends Sq1 {
       console.log(rows);
     }
   }
-};	
+};
