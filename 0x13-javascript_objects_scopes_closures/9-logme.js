02052b249d4a7a2d3b7d7b456486c359853a1352
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-15 22:16:01 +0000

    8. Esrever

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,16 +1,8 @@
-#!/usr/bin/node
+#!usr/bin/node
 
-// script that prints the number of arguments already prnted and the new argument value
-
-function * generator () {
-  let idx = 0;
-  while (true) {
-    yield idx++;
-  }
-}
-
-const gen = generator();
+let i = 0;
 
 exports.logMe = function (item) {
-  console.log(gen.next().value + ': ' + item);
+  console.log(`${i}: ${item}`);
+  i++;
 };
