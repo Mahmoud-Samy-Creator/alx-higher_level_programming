4e6fdc3b0641c2e65bdb70c72c2ad22b55400257
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-15 20:58:38 +0000

    2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-// script that creates a class.
-
+/* A class create a rectangle object */
 module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+	constructor (w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
 };
