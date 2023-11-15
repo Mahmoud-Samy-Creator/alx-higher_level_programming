3309dd7cf7b94763d03d0b4f62c56e9e32fd0d54
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-15 21:32:32 +0000

    5. Square #0

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
+/* A class Square that defines a square and inherits from Rectangle */
+
 const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
