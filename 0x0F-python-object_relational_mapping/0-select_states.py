2bbb38f69f9d13a703f030e5e0b99e126378d1f8
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-18 20:33:16 +0000

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,17 +8,17 @@ the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-user = sys.argv[1]
-password = sys.argv[2]
-database_name = sys.argv[3]
-host = "localhost"
-port = 3306
-
 if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    host = "localhost"
+    port = 3306
+
     db = MySQLdb.connect(host=host,
-                         user=user,
-                         passwd=password,
-                         db=database_name)
+                            user=user,
+                            passwd=password,
+                            db=database_name)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDERED BY id")
     rows = cursor.fetchall()
