d1d8345915f1405450b01d788053b3f06121bc1e
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-19 13:26:02 +0000

    3. SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+a script that lists all states from
+the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+    host = "localhost"
+    port = 3306
+
+    db = MySQLdb.connect(host=host,
+                         user=user,
+                         passwd=password,
+                         db=database_name,
+                         port=port)
+
+    query = """ SELECT * FROM states
+          WHERE name LIKE BINARY %s
+          ORDER BY id ASC """
+
+    cursor = db.cursor()
+    cursor.execute(query, (f"{state_name_searched}%",))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
