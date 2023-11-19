ff45eb3b53bc055b42a9eaad41840f656bd0b3fc
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-19 15:47:33 +0000

    4. Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-
 """
-Script that lists all cities from the database hbtn_0e_4_usa
+Write a script that lists
+all states from the database
+hbtn_0e_0_usa
 """
 
-import MySQLdb
-from sys import argv
 
-# The code should not be executed when imported
-if __name__ == '__main__':
-    # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
-
-    query = "select cities.id, cities.name ,\
-            states.name from cities inner join \
-            states on states.id= cities.state_id;"
-
-    cur = db.cursor(query)
-    cur.execute()
-
+    cur = db.cursor()
+    cmd = "SELECT cities.id, cities.name, states.name FROM cities\
+            INNER JOIN states ON states.id=cities.state_id;"
+    cur.execute(cmd)
     rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    # Clean up process
-    cur.close()
-    db.close()
+    for row in rows:
+        print(row)
