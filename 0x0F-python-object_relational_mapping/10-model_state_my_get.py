2fa9df52e76bc2165e4ae73d372ab1f31f77486e
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-20 21:20:05 +0000

    10. Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+A script that prints the State object
+with the name passed as argument
+from the database hbtn_0e_6_usa
+"""
+
+# Import needed modules & methods
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+# Importing the script
+if __name__ == "__main__":
+    # Create engine
+    engine = create_engine('mysql+mysqdb://{}:{}@localhost:3306/{}'.format(
+                            argv[1], argv[2], argv[3]))
+
+    # Stablishing session
+    Session = sessionmaker(bind=engine)
+
+    # Create table by metadata
+    Base.metadata.create_all(engine)
+
+    # create session
+    session = Session()
+
+    # Apply query
+    states = session.query(State) \
+                    .filter(State.name == argv[4]).one_or_none()
+
+    # print state if
+    if states is None:
+        print("Not founf")
+    else:
+        print(states.id)
+
+    # session closing
+    session.close()
