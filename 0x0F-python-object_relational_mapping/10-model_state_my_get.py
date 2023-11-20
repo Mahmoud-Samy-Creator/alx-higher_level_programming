f0efd9b2d93de1bcf7d3fcca5d34780c571c0912
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-20 21:30:16 +0000

    10. Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
-
 """
-A script that prints the State object
-with the name passed as argument
-from the database hbtn_0e_6_usa
+    A script that prints the State object with the name passed as an argument
+    from hbtn_0e_6_usa
+    Username, password, dbname and name to search
+    will be passed as arguments to the script.
 """
 
-# Import needed modules & methods
+
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
 
-# Importing the script
-if __name__ == "__main__":
-    # Create engine
-    engine = create_engine('mysql+mysqdb://{}:{}@localhost:3306/{}'.format(
-                            argv[1], argv[2], argv[3]),)
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-    # Stablishing session
     Session = sessionmaker(bind=engine)
 
-    # Create table by metadata
     Base.metadata.create_all(engine)
 
-    # create session
+    # create a session
     session = Session()
 
     # extract first state
     states = session.query(State) \
-                    .filter(State.name == argv[4]).one_or_none()
+                    .filter(State.name == sys.argv[4]).one_or_none()
 
     # print state.id
     if states is None:
