992ab64a1a688b14d6d1ac72dd808a4280e7e1c2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-20 23:50:35 +0000

    14. Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-
 """
-A script that prints all City objects
-from the database hbtn_0e_14_usa
+    A script that prints all City objects from the database hbtn_0e_6_usa
+    Username, password and dbname wil be passed as arguments to the script.
 """
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+
+import sys
 from model_state import Base, State
 from model_city import City
-from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                           argv[1], argv[2], argv[3]))
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
+
+    # create a session
     session = Session()
 
     # extract all cities in a state
