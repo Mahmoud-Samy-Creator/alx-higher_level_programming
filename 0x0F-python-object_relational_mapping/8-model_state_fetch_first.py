740db6b123de1dbe67decc484fbfa6578861edab
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-20 20:05:30 +0000

    8. First state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+a script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+# Defining connection parameters
+user = argv[1]
+password = argv[2]
+database =  argv[3]
+
+if __name__ == "__main__":
+    engine = create_engine(f'mysql://{user}:{password}@localhost:3306/{database}')
+
+    # Establishing the connection
+    Session = sessionmaker(bind=engine)
+
+    # Session making
+    session = Session()
+
+    states = session.query(State).where(State.id == 1).all()
+
+    if states is None:
+        print('Nothing')
+    else:
+        print(states)
+
