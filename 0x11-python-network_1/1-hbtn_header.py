a4c33456b614c99ad1865bc708c3c2971575c70c
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-02 23:37:05 +0000

    1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Send request to url, display value of header
 
-import urllib.request
-from sys import argv
-
 if __name__ == "__main__":
+    import urllib.request
+    from sys import argv
+
     url = argv[1]
     req = urllib.request.Request(url)
 
