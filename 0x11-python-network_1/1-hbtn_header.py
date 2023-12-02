c4fb4bce7d203c7503ad109521585bcdbbd4adec
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-02 23:33:13 +0000

    1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+# Send request to url, display value of header
+
+import urllib.request
+from sys import argv
+
+url = argv[1]
+
+req = urllib.request.Request(url)
+
+with urllib.request.urlopen(req) as response:
+    print(response.info().get("X-Request-Id"))
+
+"""
+info - this returns a dictionary-like object
+that describes the page fetched,
+particularly the headers sent by the server.
+It is currently an http.client.HTTPMessage instance.
+
+Usage: responce.info().get("Key")
+"""
