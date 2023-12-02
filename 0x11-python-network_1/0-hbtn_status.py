da8a453096ef4c9ab77c5621d10b95fe9b99dc7f
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-02 23:15:45 +0000

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-# 
+# A script fetches url 'https://alx-intranet.hbtn.io/status'
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+req = urllib.request.Request(url)
+
+with urllib.request.urlopen(req) as response:
+    data = response.read()
+
+print("Body response:")
+print(f"\t- type: {type(data)}")
+print(f"\t- content: {data}")
+print(f"\t- utf8 content: {data.decode('utf-8')}")
