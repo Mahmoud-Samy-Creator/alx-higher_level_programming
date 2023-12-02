4d235155dfe50d2722e1c4745b27f35f71cc29d5
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-02 00:18:47 +0000

    3. cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display all methods that the server will exept
-curl -sI "$1" | grep Allow: | cut -d ' ' -f 2
+# Takes a URL and ndisplays all HTTP methods server accepts
+curl -sI "$1" | grep Allow: | cut -d ' ' -f 2-
