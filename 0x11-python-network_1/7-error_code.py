7490bc871f7f704f5ba6fc1c580d00d06ab264df
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-03 01:21:28 +0000

    7. Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+A Python script that takes in a URL, sends a request
+to the URL and displays the body of the response.
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+
+    status = requests.get(url)
+
+    code = status.status_code
+
+    if status >= 400:
+        print(f"Error code: {code}")
+    else:
+        print(status.text)
