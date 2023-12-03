707d334e9f3ef65f1b4b47dfc89c7cce512e0110
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-03 01:34:55 +0000

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+A Python script that takes in a letter
+and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter
+as a parameter.
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = "http://0.0.0.0:5000/search_user"
+
+    if len(argv) == 1:
+        data = {"q": ""}
+    else:
+        data = {"q": argv[1]}
+
+    req = requests.post(url, data)
+
+    if req.headers.get("content-type") != "application/json":
+        print("Not a valid JSON")
+
+    elif req.json == "":
+        print("No result")
+
+    else:
+        print("[{}] {}".format(req.json()["id"], req.json()["name"]))
