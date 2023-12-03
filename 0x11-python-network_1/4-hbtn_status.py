6cbf659dd6dea7e0c26b62cd208fd462a77cd5ae
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-03 00:39:01 +0000

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+# Fetches url using requests module
+
+if __name__ == "__main__":
+    import requests
+
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+
+    print("Body response:")
+    print(f"\t- type: {type(req.text)}")
+    print(f"\t -content: {req.text}")
