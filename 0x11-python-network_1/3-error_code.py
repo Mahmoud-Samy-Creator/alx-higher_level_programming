afa1eb48094388cbb76e2d78809b5d12610c6a4c
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-03 00:28:23 +0000

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Takes in url, email, sends a request, print the error code,
+handle urllib.error.HTTPError
+"""
+
+if __name__ == "__main__":
+    from urllib import request, error
+    from sys import argv
+
+    # Get url
+    url = argv[1]
+
+    # Reciving response
+    try:
+        with request.Request(url) as responce:
+            print(responce.read().decode("utf-8"))
+    except error.HTTPError as e:
+        print(f"Error code: {e.code}")
