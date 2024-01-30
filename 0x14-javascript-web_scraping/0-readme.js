a190bf2827b7959a837caac53c17468d0b6c6044
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-01-30 23:39:51 +0200

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,13 +7,12 @@ Creating fs [file system ] object to interact with a file
 using file system module bult-in in javascript
 */
 
-const file = require('fs');
+const fs = require('fs');
 
 /* Step [2]
 If an error occurred during the reading, print the error object
  */
-file.readFile(process.argv[2], function (err ,data)
-{
+fs.readFile(process.argv[2], function (err, data) {
     if (err) console.log(err);
     else process.stdout.write(data);
 });
