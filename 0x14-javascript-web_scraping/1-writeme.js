03b18789888f9656adf0a7b6c8797dfea8f9aa5f
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-01-31 00:01:57 +0200

    1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -12,6 +12,9 @@ using file system module bult-in in javascript
 */
 const fs = require('fs');
 
+/**
+ Use writeFile method to write to a file
+ */
 fs.writeFile(process.argv[2], process.argv[3], (err) => {
 	if (err) console.log(err);
 });
