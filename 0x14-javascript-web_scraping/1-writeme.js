1e21888c8640898c534f943cf2e00f335b5543aa
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-01-30 23:55:51 +0200

    1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,8 +4,18 @@
  * First arg is file path
  * Second arg is string
  */
+
+
+/* Step [1]
+Creating fs [file system ] object to interact with a file
+using file system module bult-in in javascript
+*/
 const fs = require('fs');
 
+
+/*
+Use writeFile method to write to a file
+ */
 fs.writeFile(process.argv[2], process.argv[3], (err) => {
 	if (err) console.log(err);
 });
