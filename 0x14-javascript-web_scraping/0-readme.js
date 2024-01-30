1accb891ed8b64986f86fe7ee1aed1d94a200860
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-01-31 00:14:44 +0200

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -14,5 +14,5 @@ const fs = require('fs');
  */
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
 	if (err) console.log(err);
-	else process.stdout.write(data);
+	else console.log(data);
 });
