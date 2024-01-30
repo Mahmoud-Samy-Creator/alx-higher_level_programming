81d6832e7203941034e5bbbf4cc34d61a3facd61
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-01-30 23:43:45 +0200

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
 
+
 /* Reads & prints file contents */
+
 
 /* Step [1]
 Creating fs [file system ] object to interact with a file
 using file system module bult-in in javascript
 */
-
 const fs = require('fs');
+
 
 /* Step [2]
 If an error occurred during the reading, print the error object
- */
+*/
 fs.readFile(process.argv[2], function (err, data) {
-    if (err) console.log(err);
-    else process.stdout.write(data);
+	if (err) console.log(err);
+	else process.stdout.write(data);
 });
